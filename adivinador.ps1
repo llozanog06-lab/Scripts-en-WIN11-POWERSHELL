eeaf7f007cae66c5f177681158a7d55ebe8c6da7
Lorenzo <llozanog06@educarex.es>
2026-04-16 20:16:44 +0200

    Correcion

--- adivinador.ps1
+++ adivinador.ps1
@@ -4,13 +4,12 @@ $contador = 0
 do{
     $respuestaUsuario = Read-Host "Intenta adivinar el numero magico: "
     $respuestaUsuarioParseada = [int]$respuestaUsuario
+    $contador ++
     if($respuestaUsuarioParseada -lt $numeroSecreto){
         Write-Host "El numero es mayor"
-        $contador ++
         Write-Host $contador
     }else{
         Write-Host "El numero es menor"
-        $contador ++
         Write-Host $contador
     }
 }while ($respuestaUsuarioParseada -ne $numeroSecreto)
